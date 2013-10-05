73b288a2ca53515779cb55b28ed8ef87b4f6f036
r00e <ryang@thoughtworks.com>
2013-10-05 13:21:22 +0800

    update DuckFact.cs

--- StrategyPatternTest/DuckFact.cs
+++ StrategyPatternTest/DuckFact.cs
@@ -6,65 +6,44 @@ namespace StrategyPatternTest
     public class DuckFact
     {
         [Fact]
-        public void should_get_Quack_method_from_super_class()
+        public void test_action_of_MallardDuck()
         {
             var mallardDuck = new MallardDuck();
+
             Assert.Equal("gua gua gua!!!", mallardDuck.Quack());
-
-            var redheadDuck = new RedheadDuck();
-            Assert.Equal("gua gua gua!!!", redheadDuck.Quack());
-
-            var rubberduck = new Rubberduck();
-            Assert.Equal("cheep cheep cheep", rubberduck.Quack());
-        }
-
-        [Fact]
-        public void should_get_Swim_method_from_super_class()
-        {
-            var mallardDuck = new MallardDuck();
             Assert.Equal("Yahoo~ The water is so cold~~", mallardDuck.Swim());
+            Assert.Equal("Hi, I am Mallard duck. WTF!!! I have a green head!!!", mallardDuck.Display());
+        }
 
+        [Fact]
+        public void test_action_of_RedHeadDuck()
+        {
             var redheadDuck = new RedheadDuck();
+            
+            Assert.Equal("gua gua gua!!!", redheadDuck.Quack());
             Assert.Equal("Yahoo~ The water is so cold~~", redheadDuck.Swim());
+            Assert.Equal("Hi I am red head duck. I have a red head~~ Haha, look at the dude who wears a green hat! He is so franks.", redheadDuck.Display());
+        }
 
+        [Fact]
+        public void test_action_of_RubberDuck()
+        {
             var rubberduck = new Rubberduck();
+            
+            Assert.Equal("cheep cheep cheep", rubberduck.Quack());
             Assert.Equal("Yahoo~ The water is so cold~~", rubberduck.Swim());
-        }
-
-        [Fact]
-        public void should_return_green_head_for_MallardDuck()
-        {
-            var mallardDuck = new MallardDuck();
-            Assert.Contains("green", mallardDuck.Display());
-        }
-
-        [Fact]
-        public void should_return_red_head_for_RedHeadDuck()
-        {
-            var redheadDuck = new RedheadDuck();
-            Assert.Contains("red", redheadDuck.Display());
-        }
-
-        [Fact]
-        public void should_return_rubber_for_rubberDuck()
-        {
-            var rubberduck = new Rubberduck();
-            Assert.Contains("rubber", rubberduck.Display());
+            Assert.Equal("I'm made of rubber.", rubberduck.Display());
         }
 
 //        [Fact]
-//        public void should_return_fly_wings_for_MallardDuck_and_RedHeadDuck()
+//        public void test_Fly_action()
 //        {
 //            var mallardDuck = new MallardDuck();
 //            Assert.Equal("I'm flying with my wings.", mallardDuck.PerformFly());
 //
 //            var redheadDuck = new RedheadDuck();
 //            Assert.Equal("I'm flying with my wings.", redheadDuck.PerformFly());
-//        }
 //
-//        [Fact]
-//        public void should_return_can_not_fly_for_RubberDuck()
-//        {
 //            var rubberduck = new Rubberduck();
 //            Assert.Equal("I cannot fly.", rubberduck.PerformFly());
 //        }
